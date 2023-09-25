6660714c9d6d0c0291e84ac27d71c227143fe146
june <ryu9663@naver.com>
2023-09-25 14:16:25 +0900

    외부에서 import를 위한 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import * as path from "path";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/styles/libs",
+          dest: "styles",
+        },
+        {
+          src: "src/styles/_libs.scss",
+          dest: "styles",
+        },
+      ],
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.tsx"),
