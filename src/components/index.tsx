18106fde2ad31f9cb68e70d2129828e8f758147b
june <ryu9663@naver.com>
2023-09-25 15:26:58 +0900

    build 명령어 변경

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
+import "./styles/_global.scss";
 
 //* Atom 컴포넌트
-export * from "./components/atoms";
+export * from "./atoms";
 
 // //* Molecule 컴포넌트
 // export * from "./components/molecules";
--- vite.config.ts
+++ vite.config.ts
@@ -1,59 +1,78 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import * as path from "path";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-import tsconfigPaths from "vite-tsconfig-paths";
-import generatePackageJson from "rollup-plugin-generate-package-json";
+// import react from "@vitejs/plugin-react";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
+// import tsconfigPaths from "vite-tsconfig-paths";
+// import generatePackageJson from "rollup-plugin-generate-package-json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    dts({
-      insertTypesEntry: true,
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/styles/libs",
-          dest: "styles",
-        },
-        {
-          src: "src/styles/_libs.scss",
-          dest: "styles",
-        },
-      ],
-    }),
-  ],
+  // plugins: [
+  //   react(),
+  //   tsconfigPaths(),
+  //   dts({
+  //     insertTypesEntry: true,
+  //   }),
+  //   viteStaticCopy({
+  //     targets: [
+  //       {
+  //         src: "src/styles/libs",
+  //         dest: "styles",
+  //       },
+  //       {
+  //         src: "src/styles/_libs.scss",
+  //         dest: "styles",
+  //       },
+  //     ],
+  //   }),
+  // ],
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, "src/components/index.tsx"),
+  //     name: "junyeol-components",
+  //     formats: ["es", "umd"],
+  //     fileName: "index",
+  //   },
+
+  //   rollupOptions: {
+  //     external: ["react", "react-dom"],
+  //     plugins: [
+  //       generatePackageJson({
+  //         outputFolder: "dist/library",
+  //         baseContents: (pkg) => ({
+  //           ...pkg,
+  //           module: "./index.js",
+  //           main: "./index.js",
+  //           types: "./index.d.ts",
+  //           scripts: undefined,
+  //           optionalDependencies: {},
+  //           eslintConfig: undefined,
+  //         }),
+  //       }),
+  //     ],
+  //   },
+  //   commonjsOptions: {
+  //     esmExternals: ["react"],
+  //   },
+  // },
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.tsx"),
-      name: "junyeol-components",
-      formats: ["es", "umd"],
+      name: "index",
       fileName: "index",
     },
-
     rollupOptions: {
-      external: ["react", "react-dom"],
-      plugins: [
-        generatePackageJson({
-          outputFolder: "dist/library",
-          baseContents: (pkg) => ({
-            ...pkg,
-            module: "./index.js",
-            main: "./index.js",
-            types: "./index.d.ts",
-            scripts: undefined,
-            optionalDependencies: {},
-            eslintConfig: undefined,
-          }),
-        }),
-      ],
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
     },
     commonjsOptions: {
       esmExternals: ["react"],
     },
   },
+  plugins: [dts()],
 });
