144f65faa21cc7765828d2b4f90db22a245aaccb
june <ryu9663@naver.com>
2023-09-25 14:52:36 +0900

    패키지 추가 설치

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
+import generatePackageJson from "rollup-plugin-generate-package-json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
