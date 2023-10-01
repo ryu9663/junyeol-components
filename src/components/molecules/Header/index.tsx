759c2151991eb5a508313911096ca79b2a1b2cf4
june <ryu9663@naver.com>
2023-10-01 14:53:55 +0900

    feat: JCL-11 header의 첫번째 child는 왼쪽끝에 붙도록 함

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,10 +1,20 @@
 import styles from "./index.module.scss";
-import { PropsWithChildren } from "react";
+import { Children, PropsWithChildren } from "react";
 
 export const Header = ({ children }: PropsWithChildren) => {
+  const childrenArray = Children.toArray(children);
+  const firstChild = childrenArray[0];
+  const otherChildren = childrenArray.slice(1);
   return (
     <header className={styles.header}>
-      <ul className={styles["header_button-list"]}>{children}</ul>
+      <ul className={styles["header_button-list"]}>
+        <li className={styles["header_logo"]}>{firstChild}</li>
+        <div className={styles["header_right-section"]}>
+          {otherChildren.map((child, index) => (
+            <li key={index}>{child}</li>
+          ))}
+        </div>
+      </ul>
     </header>
   );
 };
