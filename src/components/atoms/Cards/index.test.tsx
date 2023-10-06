37e05dd6eef4c23c0775422cef39a4f4d153f11f
june <ryu9663@naver.com>
2023-10-06 21:39:33 +0900

    feature: JCL-13 게시글 개별 카드

---
+++ src/components/atoms/Cards/index.test.tsx
@@ -0,0 +1,65 @@
+import { Card } from "@/index";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+describe("Card에 image,h3,description들이 있는지 확인", () => {
+  it("renders Card", () => {
+    render(
+      <Card
+        img={{
+          src: "https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100",
+          alt: "아무거나",
+        }}
+        title="아무거나"
+        description="암거나"
+      />
+    );
+    screen.debug();
+  });
+
+  it('check if <img src = "https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100" src = "아무거나"/>', () => {
+    render(
+      <Card
+        img={{
+          src: "https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100",
+          alt: "아무거나",
+        }}
+        title="아무거나"
+        description="암거나"
+      />
+    );
+    const imgElement = screen.getByRole("img", { name: "아무거나" });
+    expect(imgElement).toBeInTheDocument();
+  });
+
+  it("check if <h3>아무거나</h3> is in the document", () => {
+    render(
+      <Card
+        img={{
+          src: "https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100",
+          alt: "아무거나",
+        }}
+        title="아무거나"
+        description="암거나"
+      />
+    );
+    const h3Element = screen.getByRole("heading", { name: "아무거나" });
+
+    expect(h3Element).toBeInTheDocument();
+  });
+
+  it("check if <div>'암거나'</div> is in the document", () => {
+    render(
+      <Card
+        img={{
+          src: "https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100",
+          alt: "아무거나",
+        }}
+        title="아무거나"
+        description="암거나"
+      />
+    );
+    const divElement = screen.getByText("암거나");
+
+    expect(divElement).toBeInTheDocument();
+  });
+});
--- src/components/atoms/Cards/index.tsx
+++ src/components/atoms/Cards/index.tsx
@@ -1,52 +1,32 @@
 import styles from "./index.module.scss";
 
 interface ImageProps {
-  url: string;
+  src: string;
   alt: string;
 }
 
-export type DescriptionFontSizeType = "small" | "normal" | "large";
 interface CardProps {
   img: ImageProps;
-  description: {
-    fontSize: DescriptionFontSizeType;
-    content: string;
-  };
+  title: string;
+  description: string;
   boxShadow?: boolean;
   onClick?: () => void;
 }
 export const Card = ({
   img,
+  title,
   description,
   boxShadow = true,
   onClick,
 }: CardProps) => {
-  const fontSizeClass = (() => {
-    switch (description.fontSize) {
-      case "small": {
-        return "font-size-small";
-      }
-      case "normal": {
-        return "font-size-normal";
-      }
-      case "large": {
-        return "font-size-large";
-      }
-      default: {
-        return "font-size-normal";
-      }
-    }
-  })();
-
   return (
     <div
       className={`${styles.card} ${boxShadow && styles["card_box-shadow"]}`}
       onClick={onClick}
     >
-      <img src={img.url} alt={img.alt} width={340} height={340} />
-      <div className={`${styles["card_description"]} ${styles[fontSizeClass]}`}>
-        {description.content}
-      </div>
+      <img src={img.src} alt={img.alt} width={340} height={340} />
+      <h3 className={`${styles["card_title"]}`}>{title}</h3>
+      <div className={`${styles["card_description"]}`}>{description}</div>
     </div>
   );
 };
