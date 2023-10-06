2f87fcf5335572551ac3583def225743cfa39e81
june <ryu9663@naver.com>
2023-10-06 21:43:43 +0900

    이미지 크기도 검사

--- src/components/atoms/Cards/index.test.tsx
+++ src/components/atoms/Cards/index.test.tsx
@@ -62,4 +62,20 @@ describe("Card에 image,h3,description들이 있는지 확인", () => {
 
     expect(divElement).toBeInTheDocument();
   });
+
+  it("check image의 width=340, height=340이 유지되는지 ", () => {
+    render(
+      <Card
+        img={{
+          src: "https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100",
+          alt: "아무거나",
+        }}
+        title="아무거나"
+        description="암거나"
+      />
+    );
+    const imgElement = screen.getByRole("img", { name: "아무거나" });
+    expect(imgElement).toHaveAttribute("width", "340");
+    expect(imgElement).toHaveAttribute("height", "340");
+  });
 });
