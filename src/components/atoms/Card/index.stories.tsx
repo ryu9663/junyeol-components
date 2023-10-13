e66b1fc44ab91170e99800073691543c700024ee
june <66232436+ryu9663@users.noreply.github.com>
2023-10-13 19:30:39 +0900

    feat: JCL-20 카드 레이아웃 변경 및 global.css에 h1~4태그 스타일 세팅 (#25)
    * feat: JCL-20 카드 레이아웃 변경

--- src/components/atoms/Card/index.stories.tsx
+++ src/components/atoms/Card/index.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable storybook/prefer-pascal-case */
 import type { Meta, StoryObj } from "@storybook/react";
 import { Card } from ".";
 
@@ -49,27 +50,28 @@ type Story = StoryObj<typeof Card>;
 export const Default: Story = {
   render: () => {
     return (
-      <>
+      <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
         <Card
           Thumbnail={
             <img
               width={240}
               height={240}
-              src="https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100"
+              src="https://junesdevlog-s3.s3.ap-northeast-2.amazonaws.com/portfolio_profile.png"
               alt="아무거나"
             />
           }
-          title="Next App router에서 use client가 CSR을 뜻하지는 않는다."
-          description={
-            "분명히 서버컴포넌트로 렌더링되는건 아닌데,, use client를 사용하면 CSR로 된다는 뜻이 아닌가? 하고 공식문서를 보았다. 공식문서에서 클라이언트 컴포넌트 (use client) 가 어떻게 렌더링 되는지 잘 작성되어 있다."
-          }
+          title="프론트엔드 개발자 류준열 이력서ㄹㅁ;ㅐㅑ디ㅏ로ㅟㅏㄹㅇ누ㅠㅣㅏ무지다구2ㅣㅏㅈ둠ㅎㄴㄱㅇㅋ,ㅡ류춮ㅌ,ㅡㄴ디르미ㅏㅈㄷㅎ구이ㅏㅍ쿠 ㅣㅏㄷㅁㄴ"
+          description={`안녕하세요. 프론트엔드 개발자 류준열입니다.
+
+            현실의 문제를 해결할 수 있고, 더 나은 해결책을 제시할 수 있는 개발자가 되고자 합니다.
+            
+            감사합니다.`}
         />
-      </>
+      </div>
     );
   },
 };
 
-// eslint-disable-next-line storybook/prefer-pascal-case
 export const 썸네일에_div태그_넣음: Story = {
   render: () => {
     return (
@@ -82,6 +84,60 @@ export const 썸네일에_div태그_넣음: Story = {
           }
         />
       </>
+    );
+  },
+};
+
+export const 카드_여러개: Story = {
+  render: () => {
+    return (
+      <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
+        <Card
+          Thumbnail={
+            <img
+              width={240}
+              height={240}
+              src="https://junesdevlog-s3.s3.ap-northeast-2.amazonaws.com/portfolio_profile.png"
+              alt="아무거나"
+            />
+          }
+          title="프론트엔드 개발자 류준열 이력서ㄹㅁ;ㅐㅑ디ㅏ로ㅟㅏㄹㅇ누ㅠㅣㅏ무지다구2ㅣㅏㅈ둠ㅎㄴㄱㅇㅋ,ㅡ류춮ㅌ,ㅡㄴ디르미ㅏㅈㄷㅎ구이ㅏㅍ쿠 ㅣㅏㄷㅁㄴ"
+          description={`안녕하세요. 프론트엔드 개발자 류준열입니다.
+
+            현실의 문제를 해결할 수 있고, 더 나은 해결책을 제시할 수 있는 개발자가 되고자 합니다.
+            
+            감사합니다.`}
+        />
+        <Card
+          Thumbnail={
+            <img
+              width={240}
+              height={240}
+              src="https://www.datocms-assets.com/107137/1696908548-2023-10-10-12-28-59.png"
+              alt="아무거나"
+            />
+          }
+          title="Next App router에서 use client가 CSR을 뜻하지는 않는다."
+          description={
+            "분명히 서버컴포넌트로 렌더링되는건 아닌데,,alksdjfklasdjglkasdjflkㅏㅣ@@@@@@@@@@@@@ㄱ3ㅏㄷㄹ미ㅓ캬ㅐㅣㅏ3ㅡㅁㄱㄴㄸㄹㅋㅌ이ㅏ류ㅡ파ㅣㅂ323ㅈ믑기ㅏㅁ@ㅣㅏ@@@@#%ㅆ$ㄲㅎㄴ류ㅖㅑㅣㅏㅜ use client를 사용하면 CSR로 된다는 뜻이 아닌가? 하고 공식문서를 보았다. 공식문서에서 클라이언트 컴포넌트 (use client) 가 어떻게 렌더링 되는지 잘 작성되어 있다."
+          }
+        />
+
+        <Card
+          Thumbnail={
+            <img
+              width={240}
+              height={240}
+              src="https://webudding.com/_next/image/?url=https%3A%2F%2Fd29hudvzbgrxww.cloudfront.net%2Fpublic%2Fproduct%2F20220905144129-ee57741b-6057-4a1a-8531-8e6dcb6315f7.jpg&w=3840&q=100"
+              alt="아무거나"
+            />
+          }
+          title="Next App router에서 use client가 CSR을 뜻하지는 않는다."
+          description={
+            "ㅣ만오하ㅣㅓ어롸ㅣㅎㅁ저ㅟㅏㄷㅎㄱㄴ울큩차ㅣ프3점ㅅ대ㅑㅎㄱㅇㄴㅋ리ㅠㅏ츹.ㅍ, 재ㅑ머3딘랑흨륱,ㅊㅍ .분명히 서버컴포넌트로 렌더링되는건 아닌데,, use client를 사용하면 CSR로 된다는 뜻이 아닌가? 하고 공식문서를 보았다. 공식문서에서 클라이언트 컴포넌트 (use client) 가 어떻게 렌더링 되는지 잘 작성되어 있다."
+          }
+        />
+      </div>
     );
   },
 };
--- src/components/atoms/Card/index.tsx
+++ src/components/atoms/Card/index.tsx
@@ -25,8 +25,12 @@ export const Card = ({
       onClick={onClick}
     >
       {Thumbnail}
-      <h3 className={`${styles["card_title"]}`}>{title}</h3>
-      <div className={`${styles["card_description"]}`}>{description}</div>
+      <div className={styles["card_content"]}>
+        <h3 className={`${styles["card_content-title"]}`}>{title}</h3>
+        <div className={`${styles["card_content-description"]}`}>
+          {description}
+        </div>
+      </div>
     </div>
   );
 };
