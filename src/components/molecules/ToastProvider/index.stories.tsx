388a0e85e738ca0d12a5677f70b58cba0596e04f
june <ryu9663@naver.com>
2023-10-16 15:24:53 +0900

    fix lint

--- src/components/molecules/ToastProvider/index.stories.tsx
+++ src/components/molecules/ToastProvider/index.stories.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable storybook/prefer-pascal-case */
+ 
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { ToastProvider } from ".";
