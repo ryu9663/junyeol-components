8c8d1f6dd662ea4a4db9a0052d33dda27105aba5
june <ryu9663@naver.com>
2023-11-29 22:46:41 +0900

    backdrop 초기세팅

---
+++ src/components/atoms/Backdrop/index.stories.tsx
@@ -0,0 +1,22 @@
+import { Backdrop } from "@/components/atoms/Backdrop";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Backdrop> = {
+  title: "atoms/Backdrop",
+  component: Backdrop,
+
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Backdrop>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <>
+        <Backdrop />
+      </>
+    );
+  },
+};
---
+++ src/components/atoms/Backdrop/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.scss";
+
+export const Backdrop = () => {
+  return <div className={styles.backdrop}>Backdrop</div>;
+};
