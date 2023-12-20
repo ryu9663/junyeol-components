d8306652f11abc901818204500f1839fbd197b54
june <66232436+ryu9663@users.noreply.github.com>
2023-12-20 16:39:07 +0900

    feat: JCL-49 table copy button (#57)

--- src/components/molecules/Table/HoverTableCell.tsx
+++ src/components/molecules/Table/HoverTableCell.tsx
@@ -1,10 +1,19 @@
 import { PropsWithChildren, useEffect, useRef, useState } from "react";
 import styles from "./HoverTableCell.module.scss";
+import { useToast } from "@/index";
+import nodeToString from "react-node-to-string";
+import { Copy } from "react-feather";
 
 export interface HoverTableCellProps extends PropsWithChildren {
   maxWidth: number;
+  copiable: boolean;
 }
-export const HoverTableCell = ({ children, maxWidth }: HoverTableCellProps) => {
+export const HoverTableCell = ({
+  children,
+  maxWidth,
+  copiable,
+}: HoverTableCellProps) => {
+  const toast = useToast();
   const [isHover, setIsHover] = useState(false);
   const [isLongText, setIsLongText] = useState(false);
   const beforeHoverDivRef = useRef<HTMLDivElement>(null);
@@ -19,6 +28,24 @@ export const HoverTableCell = ({ children, maxWidth }: HoverTableCellProps) => {
     }
   }, []);
 
+  const copy = async (text: string) => {
+    await navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        toast({
+          type: "success",
+          children: "내용을 복사했어요.",
+          holdTime: 3000,
+        });
+      })
+      .catch(() =>
+        toast({
+          type: "fail",
+          children: "복사를 실패했어요.",
+          holdTime: 3000,
+        })
+      );
+  };
   return (
     <>
       {isLongText && isHover ? (
@@ -27,6 +54,14 @@ export const HoverTableCell = ({ children, maxWidth }: HoverTableCellProps) => {
           className={styles["table-cell-hover"]}
         >
           {children}
+          {copiable && (
+            <button
+              data-testid="copybtn-testid"
+              onClick={() => copy(nodeToString(children))}
+            >
+              <Copy />
+            </button>
+          )}
         </div>
       ) : (
         <div
