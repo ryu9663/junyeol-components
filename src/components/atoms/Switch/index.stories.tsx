a02b17f84dc591705800cee643225428c271e06c
june <66232436+ryu9663@users.noreply.github.com>
2024-06-26 00:33:30 +0900

    feat: JCL-60 Switch (#78)
    * feat: JCL-60 Switch

---
+++ src/components/atoms/Switch/index.stories.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable storybook/prefer-pascal-case */
+import type { Meta, StoryObj } from "@storybook/react";
+import { Switch } from ".";
+import { useState } from "react";
+
+const meta: Meta<typeof Switch> = {
+  title: "atoms/Switch",
+  component: Switch,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Switch>;
+
+export const Default: Story = {
+  render: (args) => {
+    const [isChecked, setIsChecked] = useState(false);
+    return (
+      <>
+        checked: {isChecked.toString()}
+        <Switch
+          {...args}
+          value={isChecked}
+          onChange={() => setIsChecked(!isChecked)}
+        />
+      </>
+    );
+  },
+};
