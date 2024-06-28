66f8dc59aabab47f0c357b2a689ef838a3b7a413
june <66232436+ryu9663@users.noreply.github.com>
2024-06-28 23:27:51 +0900

    feat: JCL-61 푸터에 연도를 자동 계산 기능 (#79)

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,12 +1,13 @@
 import styles from "./index.module.scss";
 export const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className={styles.footer_wrapper}>
       <address>
         Phone : 010 2726 9663 <br />
         Email : ryu9663@naver.com
       </address>
-      Copyright 2023. 류준열. All rights reserved.
+      {`Copyright ${year}. 류준열. All rights reserved.`}
     </footer>
   );
 };
